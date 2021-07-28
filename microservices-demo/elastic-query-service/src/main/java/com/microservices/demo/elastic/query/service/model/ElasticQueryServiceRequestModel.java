f3c979fcd0601fdfec118b0751f93000614000ce
Le Long <longromsay@gmail.com>
2021-07-28 12:58:47 +0700

    Creating validation and controller device

--- microservices-demo/elastic-query-service/src/main/java/com/microservices/demo/elastic/query/service/model/ElasticQueryServiceRequestModel.java
+++ microservices-demo/elastic-query-service/src/main/java/com/microservices/demo/elastic/query/service/model/ElasticQueryServiceRequestModel.java
@@ -6,11 +6,14 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotEmpty;
+
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class ElasticQueryServiceRequestModel {
     private String id;
+    @NotEmpty
     private String text;
 }
