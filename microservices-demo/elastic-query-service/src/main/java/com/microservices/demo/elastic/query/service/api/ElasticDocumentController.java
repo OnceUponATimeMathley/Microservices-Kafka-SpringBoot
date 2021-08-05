5ba52f09eec52ede5ace695e0af044aa08e95a8c
Le Long <longromsay@gmail.com>
2021-08-05 15:01:51 +0700

    Added load balancer client side

--- microservices-demo/elastic-query-service/src/main/java/com/microservices/demo/elastic/query/service/api/ElasticDocumentController.java
+++ microservices-demo/elastic-query-service/src/main/java/com/microservices/demo/elastic/query/service/api/ElasticDocumentController.java
@@ -11,6 +11,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -30,6 +31,9 @@ public class ElasticDocumentController {
     public ElasticDocumentController(ElasticQueryService queryService) {
         this.elasticQueryService = queryService;
     }
+
+    @Value("${server.port}")
+    private String port;
 
     @Operation(summary = "Get all elastic documents!")
     @ApiResponses(value = {
@@ -107,7 +111,7 @@ public class ElasticDocumentController {
                 ElasticQueryServiceResponseModel.builder()
                 .text(elasticQueryServiceRequestModel.getText())
                 .build();
-        LOG.info("Elasticsearch returned {} of documents", response.size());
+        LOG.info("Elasticsearch returned {} of documents on port {}", response.size(), port);
         return ResponseEntity.ok(response);
     }
 
