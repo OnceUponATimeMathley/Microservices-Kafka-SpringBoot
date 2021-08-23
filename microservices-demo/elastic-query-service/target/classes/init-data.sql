1f4d266045b5268f8be525800e06f1f434a54285
Le Long <longromsay@gmail.com>
2021-08-23 12:49:52 +0700

    Configuring web client security with Oauth2 and OpenId

--- microservices-demo/elastic-query-service/target/classes/init-data.sql
+++ microservices-demo/elastic-query-service/target/classes/init-data.sql
@@ -18,6 +18,8 @@ insert into documents(id, document_id)
 values ('f2b2d644-3a08-4acb-ae07-20569f6f2a01', 1429356622665691142);
 insert into documents(id, document_id)
 values ('90573d2b-9a5d-409e-bbb6-b94189709a19', 1429356538825613312);
+insert into documents(id, document_id)
+values ('90573d3b-915d-409e-b2b6-b94149709a19', 1429356827939115010);
 
 insert into user_permissions(user_permission_id, user_id, document_id, permission_type)
 values (uuid_generate_v4(),'a27b5784-6c01-4bf1-b59c-f5715477698d', 'c1df7d01-4bd7-40b6-86da-7e2ffabf37f7', 'READ');
@@ -34,4 +36,5 @@ values (uuid_generate_v4(), '7978bfd4-b380-4fd3-abbd-96cbeac527bf', '90573d2b-9a
 insert into user_permissions(user_permission_id, user_id, document_id, permission_type)
 values (uuid_generate_v4(), 'd5f9e66f-ed80-4215-8c59-166624e58560', 'c1df7d01-4bd7-40b6-86da-7e2ffabf37f7', 'READ');
 
-
+insert into user_permissions(user_permission_id, user_id, document_id, permission_type)
+values (uuid_generate_v4(), 'a27b5784-6c01-4bf1-b59c-f5715477698d', '90573d3b-915d-409e-b2b6-b94149709a19', 'READ');
